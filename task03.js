4b646c35b9d40f5c79329ed93e20cd81b01e463c
Md.Sakib Hossain <86706077+sakib236160@users.noreply.github.com>
2024-11-08 13:34:48 -0800

    task 03 done

---
+++ task03.js
@@ -0,0 +1,15 @@
+// Task-3
+// Display the physics marks as output.
+
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+// student.physics = 'marks';
+// console.log(student.physics);
+console.log(student.physics.marks);
